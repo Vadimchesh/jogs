231d42fba745cd9ee2ba86cdff166ebaf261b3ee
Vadimchesh <vadimlindo@gmail.com>
2021-09-10 00:42:27 +0300

    fix: dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { FC, useEffect } from "react";
 import { useActions } from "./hooks/useActions";
-import { IUser } from "./models/IUser";
 import Header from "./components/Header/Header";
 import AppRouter from "./components/AppRouter/AppRouter";
 
 const App: FC = () => {
-  const { setUser, setIsAuth } = useActions();
+  const { setIsAuth } = useActions();
 
   useEffect(() => {
     if (localStorage.getItem("auth")) {
-      setUser({ username: localStorage.getItem("username" || "") } as IUser);
       setIsAuth(true);
     }
   }, []);
